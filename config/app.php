55aea0c788ab04994fe9cabaee915422255c54ca
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-09-06 20:18:29 -0500

    up

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
 
-        Fitztrev\LaravelHtmlMinify\LaravelHtmlMinifyServiceProvider::class
+        Fitztrev\LaravelHtmlMinify\LaravelHtmlMinifyServiceProvider::class,
 
     ],
 
