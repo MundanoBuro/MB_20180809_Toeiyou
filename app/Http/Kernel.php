17f0e1dd71583f46e3fa3d0af991568ba54582c5
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-09-07 09:55:59 -0500

    DFG

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,x
+        \App\Http\Middleware\VerifyCsrfToken::class,
     ];
   
     /**
